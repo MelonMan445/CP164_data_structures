c1b18e678ca0421d9aa69953c6017b7caad7ef1d
Jonathan Laporte <85323653+MelonMan445@users.noreply.github.com>
2025-02-11 19:03:52 -0500

    Add files via upload

---
+++ Stack_array.py
@@ -0,0 +1,175 @@
+from copy import deepcopy
+
+
+class Stack:
+
+    def __init__(self):
+        """
+        -------------------------------------------------------
+        Initializes an empty stack. Data is stored in a Python list.
+        Use: s = Stack()
+        -------------------------------------------------------
+        Returns:
+            a new Stack object (Stack)
+        -------------------------------------------------------
+        """
+        self._values = []
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the stack is empty.
+        Use: b = s.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if the stack is empty, False otherwise
+        -------------------------------------------------------
+        """
+        tf = len(self._values) == 0
+        return tf
+
+        # Your code here
+    def combine(self, source1, source2):
+        """
+        -------------------------------------------------------
+        Combines two source lists into the current target list.
+        When finished, the contents of source1 and source2 are interlaced
+        into target and source1 and source2 are empty.
+        Order of source values is preserved.
+        Use: target.combine(source1, source2)
+        -------------------------------------------------------
+        Parameters:
+            source1 - an array-based list (List)
+            source2 - an array-based list (List)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        print("out")
+        for val in source1:
+            print(val)
+        while source1.is_empty() == False or source2.is_empty() == False:
+            # print(source1.is_empty())
+            # print(source2.is_empty())
+            if source1.is_empty() == False:
+                print("in1")
+                self.push(source1.pop())
+            if source2.is_empty() == False:
+                print("in2")
+                self.push(source2.pop())
+
+    def push(self, value):
+        """
+        -------------------------------------------------------
+        Pushes a copy of value onto the top of the stack.
+        Use: s.push(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a data element (?)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        self._values.append(deepcopy(value))
+        return None
+        # Your code here
+
+    def pop(self):
+        """
+        -------------------------------------------------------
+        Pops and returns the top of stack. The value is removed
+        from the stack. Attempting to pop from an empty stack
+        throws an exception.
+        Use: value = s.pop()
+        -------------------------------------------------------
+        Returns:
+            value - the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot pop from an empty stack"
+        value = self._values.pop()
+        return value
+        # Your code here
+
+    def peek(self):
+        """
+        -------------------------------------------------------
+        Returns a copy of the value at the top of the stack.
+        Attempting to peek at an empty stack throws an exception.
+        Use: value = s.peek()
+        -------------------------------------------------------
+        Returns:
+            value - a copy of the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot peek at an empty stack"
+
+        # Your code here
+        value = deepcopy(self._values[-1])
+        return value
+
+    def split_alt(self):
+        """
+        -------------------------------------------------------
+        Splits the source stack into separate target stacks with values
+        alternating into the targets. At finish source stack is empty.
+        Use: target1, target2 = source.split_alt()
+        -------------------------------------------------------
+        Returns:
+            target1 - contains alternating values from source (Stack)
+            target2 - contains other alternating values from source (Stack)
+        -------------------------------------------------------
+        """
+        target1 = Stack()
+        target2 = Stack()
+        num = 0
+
+        while len(self._values) != 0:
+
+            if num % 2 == 0:
+                target1.push(self._values.pop())
+
+            if num % 2 == 1:
+                target2.push(self._values.pop())
+            num += 1
+        return target1 , target2
+
+    def reverse(self):
+        """
+        -------------------------------------------------------
+        Reverses the contents of the source stack.
+        Use: source.reverse()
+        -------------------------------------------------------
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        temp = []
+
+        while len(self._values) != 0:
+            temp.append(self._values.pop())
+        for things in temp:
+            self._values.append(things)
+
+    def pout(self):
+        """
+        Prints all content on self on by one
+        """
+
+        for value in self._values:
+            print(value)
+
+    def __iter__(self):
+        """
+        FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the stack
+        from top to bottom.
+        Use: for v in s:
+        -------------------------------------------------------
+        Returns:
+            value - the next value in the stack (?)
+        -------------------------------------------------------
+        """
+        for value in self._values[::-1]:
+            yield value
